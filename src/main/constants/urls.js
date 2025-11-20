be9a11b7e4b3ec9b514d39c9d41be9a9be4c5de9
zatlx <103552747+zatlx@users.noreply.github.com>
2025-11-20 17:43:45 +0700

    update URLs

--- src/main/constants/urls.js
+++ src/main/constants/urls.js
@@ -1,5 +1,5 @@
-export const LATEST_MOD_RELEASE_URL = `https://api.github.com/repos/TheKing-OfTime/YandexMusicModClient/releases/latest`;
-export const LATEST_SELF_RELEASE_URL = `https://api.github.com/repos/TheKing-OfTime/YandexMusicModPatcher/releases/latest`;
+export const LATEST_MOD_RELEASE_URL = `https://api.github.com/repos/zatlx/YandexMusicModClient/releases/latest`;
+export const LATEST_SELF_RELEASE_URL = `https://api.github.com/repos/zatlx/YandexMusicModPatcher/releases/latest`;
 export const YM_RELEASE_METADATA_URL = 'https://music-desktop-application.s3.yandex.net/stable/latest.yml';
 export const YM_RELEASE_DOWNLOAD_URL = 'https://music-desktop-application.s3.yandex.net/stable/download.json';
 export const YM_SETUP_DOWNLOAD_URLS = {
